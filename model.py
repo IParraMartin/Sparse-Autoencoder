da63596a9c63d44bd3cf3c728f65f8a5f9b1ca05
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-07-11 13:22:29 +0200

    refactor: Update SparseAutoEncoder class initialization - Update the `__init__` method of the `SparseAutoEncoder` class to include additional parameters for the hidden dimensions, sparsity lambda, and sparsity target. - Modify the encoder and decoder modules to reflect the changes in the input and output dimensions. - Implement the `forward` method to pass the input through the encoder and decoder modules. - Add placeholder methods for the sparsity penalty and loss function.

--- model.py
+++ model.py
@@ -4,26 +4,57 @@ import torch.nn as nn
 
 class SparseAutoEncoder(nn.Module):
 
-    def __init__(self, in_dims):
+    def __init__(self, in_dims: int, h_dims: int, sparsity_lambda: float = 1e-4, sparsity_target: float = 0.05):
         super().__init__()
         self.in_dims = in_dims
-        self.features = in_dims * 2
+        self.h_dims = h_dims
+        self.sparsity_lambda = sparsity_lambda
+        self.sparsity_target = sparsity_target
 
+        """
+        Map the original dimensions to a higher dimensional layer of features.
+        Apply relu non-linearity to the linear transformation.
+        """
         self.encoder = nn.Sequential(
-            nn.Linear(self.in_dims, self.features),
+            nn.Linear(self.in_dims, self.h_dims),
             nn.ReLU()
         )
 
+        """
+        Map back the features to the original input dimensions.
+        Apply relu non-linearity to the linear transformation.
+        """
+        self.decoder = nn.Sequential(
+            nn.Linear(self.h_dims, self.in_dims),
+            nn.ReLU
+        )
 
-
+    """
+    We pass the original signal through the encoder. Then we pass
+    that transformation to the decoder and return both results.
+    """
     def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
+    
+    """
+    This is the sparsity penalty we are going to use (KL)
+    """
+    def sparsity_penalty(self, encoded):
+        pass
+
+    """
+    Create a custom loss that combine mean squared error (MSE) loss 
+    for reconstruction with the sparsity penalty.
+    """
+    def loss_function(self, x_hat, x, encoded):
         pass
 
 
 if __name__ == "__main__":
 
     I_DIM = 1_000_000
-    FEATURAL_H_DIMS = I_DIM * 2
     
 
 
