11897a3c006d5f9901ef10ac9e0854b087ede307
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-07-18 14:02:31 +0200

    refactor: Update `sae.py` to include device selection for training

--- sae.py
+++ sae.py
@@ -9,6 +9,7 @@ from torchsummary import summary
 import os
 import certifi
 
+
 # ensure that Python uses up-to-date CA certificates when making secure connections and prevent errors
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
